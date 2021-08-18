ecdc4412045edac4dd42928d9599cb0e6a0a035c
dimanno <dimanno.dev@gmail.com>
2021-08-18 12:58:05 +0300

    classWork4 finish

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-// import {Form} from "./Form/Form";
 import {ControledForm} from "./Controled-form/Controled.form";
 
 
 function App() {
   return (
       <div className="main">
-        {/*<Form/>*/}
           <ControledForm/>
       </div>
   );
